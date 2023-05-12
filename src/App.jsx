792118d70894625f090527a437c8b3afc55837e4
ykshprskyi <ykashperskiy162@gmail.com>
2023-05-12 23:25:52 +0300

    router changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/",
+        path: "/Music",
         element: <HomePage />,
       },
       {
